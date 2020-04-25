d691ce27d613ca1cf4b4bc403bcee10f70d44723
Alex Kharouk <akharoukpp@gmail.com>
2020-04-25 16:26:05 +0100

    set up some delicious apples scene

--- Pig.gd
+++ Pig.gd
@@ -1,19 +1,29 @@
 extends Area2D
 
-export(int) var speed = 150
+export(int) var speed = 100
+var moving = false
 
 onready var animationPlayer = $AnimationPlayer
+onready var pigSprite = $Sprite
 
 func _process(delta):
+	moving = false
 	if Input.is_action_pressed("ui_right"):
+		pigSprite.flip_h = false
 		move(speed, 0, delta)
 	if Input.is_action_pressed("ui_left"):
+		pigSprite.flip_h = true
 		move(-speed, 0, delta)
 	if Input.is_action_pressed("ui_up"):
 		move(0, -speed, delta)
 	if Input.is_action_pressed("ui_down"):
 		move(0, speed, delta)
+	if moving:
+		animationPlayer.play("Run")
+	else:
+		animationPlayer.play("Idle")
 
 func move(xspeed, yspeed, delta):
+	moving = true
 	position.x += xspeed * delta
 	position.y += yspeed * delta
