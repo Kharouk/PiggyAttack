cced67338fc65743702ea89f814785452f7e509c
Alex Kharouk <akharoukpp@gmail.com>
2020-04-25 16:32:43 +0100

    when pig collides with anything, absorb it for nutrition

--- Pig.gd
+++ Pig.gd
@@ -18,12 +18,21 @@ func _process(delta):
 		move(0, -speed, delta)
 	if Input.is_action_pressed("ui_down"):
 		move(0, speed, delta)
+		
 	if moving:
 		animationPlayer.play("Run")
 	else:
 		animationPlayer.play("Idle")
+	# another way of handling queue_free if not using signal (or potentially dealing with multiple overlapped areas)
+	# var areas = get_overlapping_areas()
+	# for area in areas:
+		# area.queue_free()
 
 func move(xspeed, yspeed, delta):
 	moving = true
 	position.x += xspeed * delta
 	position.y += yspeed * delta
+
+
+func _on_Pig_area_entered(area):
+	area.queue_free()
--- notes.md
+++ notes.md
@@ -1,4 +1,5 @@
 # PiggyAttack - 1 Bit Game for Godot
 - Use Area2D (instead of the other collision models) if you just want to check to see if two 2d areas overlap (like a pig over an apple)
 - Can use sprite sheets, just import sprite sheet as sprite and use the animation tab to set width or height frames.
-- Area2D needs a sprite and a CollisionShape. 
+- Area2D needs a sprite and a CollisionShape.
+- when using signals to detect if something entered the pig area, you can use area.queue_free() to destroy the thing that gets entered 
