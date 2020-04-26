85ea8283c4c880c723c712409fdcaf14965e92b2
Alex Kharouk <akharoukpp@gmail.com>
2020-04-26 17:35:58 +0100

    Update instructions logic

--- World.gd
+++ World.gd
@@ -1,11 +1,17 @@
 extends Node
 
-
+var title = null
 func _ready():
 	var children = get_children()
+	title = $Instructions
 	for child in children:
 		print(child.name)
 		
 
 func _process(delta):
 	pass
+
+
+func _on_Pig_area_entered(area):
+	title.hide()
+	pass # Replace with function body.
